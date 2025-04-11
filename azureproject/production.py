c8b89c2d12fb939e5d35dde4fdb9ae2782aaee51
sandraff3 <137799506+sandraff3@users.noreply.github.com>
2025-04-11 07:36:21 +0000

    Configure Azure database connecton3

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,4 +1,5 @@
 import os
+
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
     dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
